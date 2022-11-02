fdf672a3f2ff4c7a59ca0c5b7b75033a062cd4cb
Rayane-AK <akliouat.samy@gmail.com>
2022-11-02 20:54:34 +0100

    Config

---
+++ tailwind_vite/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App flex justify-center items-center h-screen">
+      <h1 className="bg-red-600 p-2 animate-pulse rounded">
+        Voila ! ça marche
+      </h1>
+    </div>
+  );
+}
+
+export default App;
